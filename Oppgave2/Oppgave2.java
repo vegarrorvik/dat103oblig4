85faceb4b2f3aedc10f6edcf1a5d59dcd8d37aca
ady006 <dyrstad.a@gmail.com>
2015-11-11 10:52:19 +0100

    Gjort ting

---
+++ Oppgave2/Oppgave2.java
@@ -0,0 +1,13 @@
+import java.lang.Thread;
+import java.util.concurrent.Semaphore;
+
+public class Oppgave2{
+    public static void main(String args){
+
+        Thread thread = new Thread();
+        int in = 0;
+        int out = 0;
+
+
+    }
+}
